e3f527595d742a1671e1dda2e15e2db3a886e29f
Teej <tyrush1911@outlook.com>
2023-11-10 21:16:26 -0500

    skeletons for routes and main js files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const values = require("../db/values");
+
+router.get('/notes' , (req,res) => {
+
+})
+
+router.post ("/notes" , (req,res) => {
+    
+})
+
