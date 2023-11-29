fe82d40989e0e06a151164c671a9c6c42d76da3f
Teej <tyrush1911@outlook.com>
2023-11-29 12:01:24 -0500

    uuid conflict solved

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
+app.use('/', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
